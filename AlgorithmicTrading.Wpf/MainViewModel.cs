b79ac7ac5ae2c2eb8ccd2f326433e4c7be6917c6
Fredrik Skeel Løkke <f.loekke@gmail.com>
2015-09-19 17:38:25 +0200

    foo

--- AlgorithmicTrading.Wpf/MainViewModel.cs
+++ AlgorithmicTrading.Wpf/MainViewModel.cs
@@ -8,6 +8,7 @@ using Abt.Controls.SciChart.Model.DataSeries;
 using Abt.Controls.SciChart.Visuals.RenderableSeries;
 using System.Windows.Media;
 using Abt.Controls.SciChart.Visuals.Axes;
+using System.Collections.Generic;
 
 namespace AlgorithmicTrading.Wpf
 {
@@ -39,24 +40,34 @@ namespace AlgorithmicTrading.Wpf
         {
             XVisibleRange = new IndexRange(0, 100);
 
-            var quotes = from symbol in this.WhenAnyValue(x => x.SymbolTextBox).Throttle(TimeSpan.FromSeconds(3))
+            var quotes = from symbol in this.WhenAnyValue(x => x.SymbolTextBox).Throttle(TimeSpan.FromSeconds(1))
                          where !string.IsNullOrEmpty(symbol)
                          from quote in YahooDataProvider.LiveFeed(symbol)  // TODO: should we do a join instead?
                          where quote != null
-                         select quote;
+                         select new { Quote = quote, Symbol = symbol };
 
-            
-
-            var series = new XyDataSeries<DateTime, double>();
-            series.SeriesName = "aapl";
-            
-            ChartSeries.Add(new ChartSeriesViewModel(series, new FastLineRenderableSeries()));
+            var seriesDic = new Dictionary<string, XyDataSeries<DateTime, double>>();
 
             quotes.SubscribeOn(RxApp.TaskpoolScheduler)
                 .ObserveOn(RxApp.MainThreadScheduler)
                 .Do(Console.WriteLine)
-                .Subscribe(quote =>
+                .Subscribe(quoteAndSymbol =>
                 {
+                    XyDataSeries<DateTime, double> series;
+
+                    if (seriesDic.ContainsKey(quoteAndSymbol.Symbol))
+                    {
+                        series = seriesDic[quoteAndSymbol.Symbol];
+                    }
+                    else
+                    {
+                        series = new XyDataSeries<DateTime, double>();
+                        series.SeriesName = quoteAndSymbol.Symbol;
+                        ChartSeries.Add(new ChartSeriesViewModel(series, new FastLineRenderableSeries()));
+                        seriesDic[quoteAndSymbol.Symbol] = series;
+                    }
+
+                    series.Append(DateTime.Now, quoteAndSymbol.Quote.Ask);
 
                     if (XVisibleRange != null && series.Count > XVisibleRange.Max)
                     {
@@ -65,7 +76,6 @@ namespace AlgorithmicTrading.Wpf
                         XVisibleRange = newRange;
                     }
 
-                    series.Append(DateTime.Now, quote.Ask);
                 });
 
         }
